bd34761101e7b11dbbba26624b8d2f82f0ca1ba6
dakamihir <dakamihir@gmail.com>
2021-09-14 00:47:04 +0530

    homepage/auth

--- lib/Home/profile.dart
+++ lib/Home/profile.dart
@@ -6,7 +6,9 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      child : Center(
+        child: Text('Profile')
+      )
     );
   }
 }
