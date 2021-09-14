325e1e3a93750d74b3b040c6774fa76c31bd724e
Nishank00 <nishanksidhpura@gmail.com>
2021-09-14 12:27:43 +0530

    Login Fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'Home/homepage.dart';
 import 'Home/profile.dart';
 import 'splashpage.dart';
 
@@ -10,17 +12,49 @@ void main() async {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Prasad - Letfover food management system',
+//       theme: ThemeData(
+//         fontFamily: "ProductSans",
+//       ),
+//       home: SplashPage(),
+//     );
+//   }
+// }
+
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  User? user;
+
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  getCurrentUser() {
+    user = _auth.currentUser;
+    print('user id : $user');
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getCurrentUser();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Prasad - Letfover food management system',
-      theme: ThemeData(
-        fontFamily: "ProductSans",
-      ),
-      home: SplashPage(),
+      home: user != null ? HomePage() : SplashPage(),
     );
   }
 }
